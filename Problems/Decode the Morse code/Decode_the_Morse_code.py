846338cf583851936348097fe6c981b6598e3650
unknown <grigor.barseghyan111@gmail.com>
2019-09-14 16:03:02 +0400

    Added problems

--- Problems/Decode the Morse code/Decode_the_Morse_code.py
+++ Problems/Decode the Morse code/Decode_the_Morse_code.py
@@ -1,14 +1,19 @@
 def decodeMorse(morse_code):
     d = {'.-': "A", '-...': "B", '-.-.': "C", '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H', '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P', '--.-': 'Q','...---...':'SOS', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X', '-.--': 'Y', '--..': 'Z','-.-.--':'!','.-.-.-':'.'}
-    list1=morse_code.split("   ")
+    #return " ".join(''.join([[d[x] for x in morse.split()] for morse in morse_code.split('   ')]))
+    #morse=list()
+    print([[d[x] for x in morse.split()] for morse in morse_code.split('   ')])
+    return " ".join([''.join([d[x] for x in morse.split()]) for morse in morse_code.split('   ')])
+    """list1=morse_code.split("   ")
     for x in range(len(list1)):
         list1[x]=list1[x].split()
     for x in range(len(list1)):
-        for y in range(len(list1[x])):
-            list1[x][y]=d[list1[x][y]]
+    for y in range(len(list1[x])):
+        list1[x][y]=d[list1[x][y]]
     for x in range(len(list1)):
         list1[x]="".join(list1[x])
     morse_code=" ".join(list1).strip()
-    return(morse_code)
+    return(morse_code)"""
+    
 n=decodeMorse('...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-')
 print(n)
--- Problems/Dubstep/Dubstep.py
+++ Problems/Dubstep/Dubstep.py
@@ -1,35 +1,6 @@
 def song_decoder(song):
-    song1=str()
-    t=0
-    x=0
-    n=len(song)
-    if n<3:
-        print(song)
-    else:
-        while x<n:
-            if song[x]!="W":
-                song1=song1+song[x]
-                x+=1
-            else:
-                while True:
-                    if song[x:x+3]=="WUB":
-                        x=x+3
-                        t+=1
-                    else:
-                        break
-                print(t)
-                if t==0:
-                    song1=song1+song[x]
-                else:
-                    song1=song1+" "
-                    t=0
-        if(song1[0]==" "):
-            song1=song1[1:]
-        n=len(song1)
-        if(song1[n-1]==" "):
-            song1=song1[:n-1]
-        return song1
-n=song_decoder("WUBAWUBBWUBC")
+    return ' '.join(song.replace('WUB'," ").split())
+n = song_decoder("WUBAWUBBWUBC")
 print(n)
 
         
